b7e05040617eb6ec8d2a8a835f24ed08fe2266a3
Siddhant Kameswar <115331356+grimsteel@users.noreply.github.com>
2024-09-10 18:00:17 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# MathXL Grade Calculator
+
+> Calculates and remembers unknown grades in MathXL.
+
+Can handle more than one unknown grade at once
---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,ts}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config
+
